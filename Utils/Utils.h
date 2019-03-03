836f02253c6d3ef9d13129c6004f8fc2ced942c9
Harelon <harel.mspc@gmail.com>
2019-03-03 10:20:55 +0200

    fixed Utils.h bug now works
    Signed-off-by: Harelon <harel.mspc@gmail.com>

--- Utils/Utils.h
+++ Utils/Utils.h
@@ -1,7 +1,15 @@
 #ifndef UTILS_LIB_H
 #define UTILS_LIB_H
 
-#include <Adafruit_NeoPixel.h>
+#include <Arduino.h>
+
+typedef enum {
+  NONE =  0,
+  RED =  16711680,
+  GREEN =  65280,
+  BLUE =  255,
+  WHITE =  16777216,
+} MainColors;
 
 class Utils
 {
@@ -11,11 +19,4 @@ class Utils
         Utils(){};
 };
 
-typedef enum {
-  NONE = Adafruit_NeoPixel::Color(0, 0, 0),
-  RED = Adafruit_NeoPixel::Color(255, 0, 0),
-  GREEN = Adafruit_NeoPixel::Color(0, 255, 0),
-  BLUE = Adafruit_NeoPixel::Color(0, 0, 255),
-  WHITE = Adafruit_NeoPixel::Color(255, 255, 255),
-} MainColors;
 #endif
